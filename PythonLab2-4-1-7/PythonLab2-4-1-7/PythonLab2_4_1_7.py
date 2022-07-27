55790b8bc086a07f9b2ee91c04c03d37a8a2b7e6
Harvey1968 <108738675+Harvey1968@users.noreply.github.com>
2022-07-27 11:40:03 +0100

    Add files via upload

---
+++ PythonLab2-4-1-7/PythonLab2-4-1-7/PythonLab2_4_1_7.py
@@ -0,0 +1,12 @@
+#Scenario
+john = 3
+mary = 4
+adam = 5
+print(john, mary, adam, sep=",", end="!\n")
+print("total_apples =", john + mary + adam, end="!\n\n")
+#Extra
+total_apples = john + mary + adam
+print("John has", john, "apples", end=",\n")
+print("Mary has", mary, "apples", end=",\n")
+print("Adam has", adam, "apples", end=",\n")
+print("Altogether they have", total_apples, "apples", end="!\n\n")
